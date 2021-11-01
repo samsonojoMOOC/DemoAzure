f22ea0a5138516d01c59f1e6873eecd286f4bf98
samson ojo <samson.ojo@gmail.com>
2021-11-01 17:37:17 +0100

    Initial Commit

---
+++ Test Suites/Google.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Google</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f62bd1d4-d5bd-4930-9628-f0233cf40348</testSuiteGuid>
+   <testCaseLink>
+      <guid>0d746a4d-023b-4c58-8818-9fadc2336f99</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/NavigateToGoogle</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
